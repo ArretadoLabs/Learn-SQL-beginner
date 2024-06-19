b99f7aadbbda8f29bf04e43cffdeac1db9d05c3d
Arretados Labs <arretadolabs@gmail.com>
2024-06-18 23:57:22 -0300

    Inserting example with more two table in INNER JOIN and examples with operator BETWEEN

--- Second-part-learning-SQL.sql
+++ Second-part-learning-SQL.sql
@@ -20,9 +20,43 @@ WHERE pad.City LIKE '%vina%' -- replace for anywhere value
 
 
 -- Example using command LEN and function TOP
-SELECT TOP 10 ppas.BusinessEntityID as 'ID of area business'
+SELECT TOP 10 ppas.BusinessEntityID as 'ID of area business' -- parameter of command return first ten values of table
 FROM Person.Password as ppas
 WHERE LEN(ppas.PasswordHash) > 42 AND ppas.BusinessEntityID > 10
+
+--======================== Using examples with operator BETWEEN ========================
+
+SELECT *
+FROM HumanResources.Employee as he
+WHERE he.OrganizationLevel BETWEEN 1 AND 3
+
+-- Others example using operator BETWEEN and INNER JOIN
+-- In example using three tables
+SELECT *
+FROM ((HumanResources.Employee as he INNER JOIN Person.Person as ppe ON he.BusinessEntityID = ppe.BusinessEntityID)
+       INNER JOIN Sales.Customer as sc ON he.BusinessEntityID = sc.CustomerID) 
+WHERE he.OrganizationNode IS NOT NULL AND he.Gender = 'm'
+
+-- Using operator BETWEEN and command IN with clausule WHERE
+SELECT pp.FirstName as 'Primeiro nome', pp.LastName as 'Último nome', he.VacationHours as 'Horas vagas'
+FROM HumanResources.Employee as he INNER JOIN Person.Person as pp ON he.BusinessEntityID = pp.BusinessEntityID
+WHERE he.VacationHours BETWEEN 10 AND 50
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
