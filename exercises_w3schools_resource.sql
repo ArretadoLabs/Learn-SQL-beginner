54eaec48c60ae615ba829a63cdfa2c21cab5ffdd
Arretados Labs <arretadolabs@gmail.com>
2024-06-20 15:23:33 -0300

    new exercise - update

--- exercises_w3schools_resource.sql
+++ exercises_w3schools_resource.sql
@@ -26,11 +26,22 @@ SELECT pp.FirstName as 'First Name', pp.LastName as 'Last Name', pp.BusinessEnti
 FROM Person.Person as pp
 ORDER BY pp.LastName ASC
 
+-- 4. From the following table write a query in SQL to return only the rows for product that have a sellstartdate that is not NULL and a productline of 'T'. 
+-- Return productid, productnumber, and name. Arranged the output in ascending order on name. 
 
+SELECT pprod.ProductID as 'Product ID', pprod.ProductNumber as 'Product Number', pprod.Name as 'Name of product'
+FROM Production.Product as pprod
+WHERE pprod.SellStartDate IS NOT NULL and pprod.ProductLine = 'T'
+ORDER BY pprod.Name ASC
 
+--5. From the following table write a query in SQL to return all rows from the salesorderheader table in Adventureworks database and calculate 
+--the percentage of tax on the subtotal have decided. 
+--Return salesorderid, customerid, orderdate, subtotal, percentage of tax column. 
+--Arranged the result set in ascending order on subtotal. 
 
-
-
+SELECT sso.SalesOrderID as 'Sales order ID', sso.SalesPersonID as 'ID sales Person', sso.SubTotal as 'Value subtotal', sso.TaxAmt as 'Tax Percent'
+FROM Sales.SalesOrderHeader as sso
+ORDER BY sso.SubTotal DESC
 
 
 
